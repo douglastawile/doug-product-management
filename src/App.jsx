3fca0d44c55b5d4f4ffb791064b42c75d16c0589
Douglas Maalongaye <douglastawile@gmail.com>
2022-07-14 17:36:59 +0000

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="container my-2">
+      <main className="row">
+        <h2>Welcome to Douglas Product Management.</h2>
+      </main>
+    </div>
+  );
+}
